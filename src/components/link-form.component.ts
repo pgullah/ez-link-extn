2c16ecf394a033234a30feb1d9e7f5cdcb1336cc
Pradeep <deepuvsdeepu@gmail.com>
2024-02-28 14:55:20 +0000

    singal refactor

--- src/components/link-form.component.ts
+++ src/components/link-form.component.ts
@@ -5,6 +5,8 @@ import { toBoolean } from '../common/utils';
 import './link-parameter-component'
 import './select.component'
 import { KeyOption, Link } from '../common/models';
+import { appState } from '../common/app-state';
+
 
 @customElement("app-link-form")
 export class FormComponent extends LitElement {
@@ -22,6 +24,18 @@ export class FormComponent extends LitElement {
         this.link = this.link || [];
         this.link.params = this.link.params || [];
         this.link.params = [...this.link.params, {}]
+    }
+
+    private hideFrom() {
+        this.dispatchEvent(new CustomEvent('hideLinkForm',{
+            bubbles: true,
+            composed: true
+        }))
+    }
+
+    private async saveForm(link: Link) {
+        await db.saveLink(this.link)
+        appState.links.value = await db.findAllLinks()
     }
 
     private advancedOptions() {
@@ -67,20 +81,20 @@ export class FormComponent extends LitElement {
 
     render() {
         return html`
-        <form class="ez-container" action="#" @submit="${async () => await db.saveLink(this.link)}">
-            <input type="hidden" name="id" value="${this.link.id} />
+        <form class="ez-container" action="#" @submit="${async () => await this.saveForm(this.link)}">
+            <input type="hidden" name="id" value="${this.link.id ?? ''} />
             <div class=" row">
                 <div class="cell"> <label>Title: </label></div>
                 <div class="cell"> <input type="text" name="title" required value="${this.link.title}" @change=${this.setInputValue(v => this.link.title = v)}/> </div>
             </div>
             <div class="row">
                 <div class="cell"> <label>URL: </label></div>
-                <div class="cell"> <input type="url" name="url" required minlength="6" value="${this.link.url}" @change=${this.setInputValue(v => this.link.title = v)}/> </div>
+                <div class="cell"> <input type="url" name="url" required minlength="6" value="${this.link.url}" @change=${this.setInputValue(v => this.link.url = v)}/> </div>
             </div>
             <div class="row">
                 <div class="cell"> <label>Method: </label></div>
                 <div class="cell">
-                    <select required @change=${this.setInputValue(v => this.link.title = v)}>
+                    <select required @change=${this.setInputValue(v => this.link.method = v as any)}>
                         ${this.buildHttpMethodOptions().map(o => html `<option value=${o.key} ?selected=${o.key == this.link.method}>${o.value}</option>`)}
                     <select>
                 </div>
@@ -96,7 +110,7 @@ export class FormComponent extends LitElement {
             <div class="row">
                 <div class="cell">
                     <button type="submit" class="save-link"> Save </button> &nbsp;
-                    <button type="reset"> Clear </button>
+                    <button @click=${this.hideFrom}> Cancel </button>
                 </div>
             </div>
         </form>
