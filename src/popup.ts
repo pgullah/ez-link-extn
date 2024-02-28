92fd99b69632588004135390a087d46be28789c9
Pradeep <deepuvsdeepu@gmail.com>
2024-02-28 12:45:00 +0000

    singal refactor

--- src/popup.ts
+++ src/popup.ts
@@ -4,38 +4,13 @@ import { ifExists, isEmpty, toBoolean } from './common/utils.js';
 import { LitElement, html, css } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 import './components/link-form.component';
-import './components/link.component'
+import './components/links.component'
 import { Link } from './common/models';
-
-
-/* @customElement("app-link-adv-opts")
-export class AdvancedOptionsComponent extends LitElement {
-    params: LinkParameter[] = []
-
-    render() {
-        const addNewParameter = () => {
-            // refresh model
-            this.params.push({ key: '', value: '', desc: '' });
-            refreshComponent("adv-parm-list", AdvancedParameterListComponent, this.params);
-        }
-        return html`
-    <div id="toggle-adv-container" class="row container" data-toggle="${toBoolean(context.toggle)}">
-        <div class="cell">
-            <div class="row">
-                <div class="cell"><button type="button" id="add-adv-param" onclick="${addNewParameter}">Add new parameter</button></div>
-            </div>
-            <div id="adv-parm-list">
-                ${this.params.length > 0 && AdvancedParameterListComponent(this.params)}
-            </div>
-        </div>
-    </div>
-    `;
-    }
-} */
+import { SignalWatcher } from '@lit-labs/preact-signals';
 
 
 @customElement("app-root")
-export class AppComponent extends LitElement {
+export class AppComponent extends SignalWatcher(LitElement) {
     @state()
     linkForm?: Link
     links?: Link[]
@@ -47,7 +22,6 @@ export class AppComponent extends LitElement {
     render() {
         db.findAllLinks().then(r => this.links = r);
         const hasLinks = this.links && this.links.length > 0;
-        // this.linkForm = {}
         return html`
         <div class="ez-container">
         <h4 id="greeting">${hasLinks ? "Your Links" : "Sorry, you don't seem to have any links. Try adding links by clicking the 'Add new link' button"}</h4>
